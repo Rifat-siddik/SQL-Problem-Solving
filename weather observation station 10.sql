935fc67aa146e95e7341dd9c513df626bbfa6cda
Abu Bakkar Siddik Rifat <125892159+Rifat-siddik@users.noreply.github.com>
2026-06-04 21:46:45 +0600

    Create weather observation station 10.sql

---
+++ weather observation station 10.sql
@@ -0,0 +1,7 @@
+-- Weather Observation Station 10
+-- https://www.hackerrank.com/challenges/weather-observation-station-10/problem?isFullScreen=true
+
+
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY REGEXP '[^aeiou]$';
